9f4f85b2abb64b20e631b8e8a9fcb0ddd221c3a3
Ryan1985 <xieyuliang1985@126.com>
2015-03-07 13:33:14 +0800

    FirstCommit

---
+++ BusinessModels/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessModels
+{
+    public class Customer
+    {
+
+        //点单
+        public Order MakeOrder(Menu mainMenu)
+        {
+            throw new NotImplementedException();
+        }
+
+        //结账
+        public Bill Pay(Order order)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+        
+
+
+    }
+}
---
+++ Presenters/LoginPresenter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Views;
+using Models;
+
+
+
+
+namespace Presenters
+{
+    public class LoginPresenter
+    {
+        public LoginPresenter(LoginView view)
+        {
+            this.View = view;
+            this.View.btnLoginClick += delegate
+            {
+                LoginPresenterModel model = new LoginPresenterModel();
+                model.LoginName = this.View.LoginName;
+                model.Password = this.View.LoginPassword;
+
+                string strResult = model.CheckUserPassword();
+                this.View.ShowMessageBox(strResult);
+                if (strResult.Contains("成功"))
+                {
+                    CashierPresenter capre = new CashierPresenter(new CashierView());
+                }
+
+            };
+
+
+            this.View.btnExitClick += delegate
+            {
+                this.View.Close();
+            };
+        }
+
+
+
+        public LoginView View { get; set; }
+
+
+
+
+
+
+    }
+}
