f8ada723b3b651ae58b4c71419ebf270dedd0679
Ryan1985 <xieyuliang1985@126.com>
2015-03-08 11:50:43 +0800

    CommitChanges

--- Models/LayoutPresenterModel.cs
+++ Models/LayoutPresenterModel.cs
@@ -21,56 +21,20 @@ namespace Models
 
 
 
-        public List<Table> Tables = new List<Table>();
+        public DataTable dtTableSeats { get; set; }
 
-        public List<Seat> Seats = new List<Seat>();
+
 
         BusinessLogic bl = new BusinessLogic();
 
 
         public LayoutPresenterModel()
         {
-            LoadLayout();
+            dtTableSeats = bl.GetFloorLayout();
+
+
         }
 
-        private void LoadLayout()
-        {
-            Tables.Clear();
-            Seats.Clear();
-            DataTable dt = new DataTable();
-            dt = bl.GetFloorLayout();
-
-            DataTable dtTables =  dt.DefaultView.ToTable(true, "TableNo").Copy();
-            for (int i = 0; i < dtTables.Rows.Count; i++)
-            {
-
-                Table t = new Table();
-                t.Length = int.Parse(dt.Rows[i]["TableLength"].ToString());
-                t.PositionX = int.Parse(dt.Rows[i]["TablePositionX"].ToString());
-                t.PositionY = int.Parse(dt.Rows[i]["TablePositionY"].ToString());
-                t.Width = int.Parse(dt.Rows[i]["TableWidth"].ToString());
-                t.TableNo = int.Parse(dt.Rows[i]["TableNo"].ToString());
-                t.SeatList = new List<Seat>();
-                DataRow[] drSeats = dt.Select("TableNo = '" + dtTables.Rows[i]["TableNo"].ToString() + "'");
-                foreach(DataRow dr in drSeats)
-                {
-                    Seat s = new Seat();
-                    s.CurrentDirection = (Seat.Direction)Enum.Parse(typeof(Seat.Direction), dr["Direction"].ToString());
-                    s.CurrentStatus = (Seat.Status)Enum.Parse(typeof(Seat.Status), dr["CurrentStatus"].ToString());
-                    s.Length = int.Parse(dr["SeatLength"].ToString());
-                    s.PositionX = int.Parse(dr["SeatPositionX"].ToString());
-                    s.PositionY = int.Parse(dr["SeatPositionY"].ToString());
-                    s.Width = int.Parse(dr["SeatWidth"].ToString());
-                    s.TableNo = int.Parse(dr["SeatNo"].ToString());
-                    s.SeatNo = int.Parse(dr["SeatNo"].ToString());
-
-                    t.SeatList.Add(s);
-                    Seats.Add(s);
-                }
-
-                Tables.Add(t);
-            }
-        }
 
 
 
--- Presenters/LayoutPresenter.cs
+++ Presenters/LayoutPresenter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Views;
-using UIEntity;
+using System.Data;
 using Models;
 
 namespace Presenters
@@ -11,14 +11,20 @@ namespace Presenters
     public class LayoutPresenter
     {
 
+        private List<Table> Tables = new List<Table>();
+        private List<Seat> Seats = new List<Seat>();
+
+
+        private LayoutPresenterModel model = new LayoutPresenterModel();
+
+
         public LayoutPresenter(LayoutView view)
         {
             this.View = view;
-
-            LayoutPresenterModel model = new LayoutPresenterModel();
-
-            this.View.PutTables(model.Tables);
-            this.View.PutSeats(model.Seats);
+            LoadLayout();
+            
+            this.View.PutTables(Tables);
+            this.View.PutSeats(Seats);
             this.View.LinkButtonClickEvent();
             this.View.ButtonClicked += delegate
             {
@@ -43,6 +49,43 @@ namespace Presenters
 
 
 
+        private void LoadLayout()
+        {
+            Tables.Clear();
+            Seats.Clear();
+            DataTable dt = model.dtTableSeats;
+
+            DataTable dtTables = dt.DefaultView.ToTable(true, "TableNo").Copy();
+            for (int i = 0; i < dtTables.Rows.Count; i++)
+            {
+
+                Table t = new Table();
+                t.Length = int.Parse(dt.Rows[i]["TableLength"].ToString());
+                t.PositionX = int.Parse(dt.Rows[i]["TablePositionX"].ToString());
+                t.PositionY = int.Parse(dt.Rows[i]["TablePositionY"].ToString());
+                t.Width = int.Parse(dt.Rows[i]["TableWidth"].ToString());
+                t.TableNo = int.Parse(dt.Rows[i]["TableNo"].ToString());
+                t.SeatList = new List<Seat>();
+                DataRow[] drSeats = dt.Select("TableNo = '" + dtTables.Rows[i]["TableNo"].ToString() + "'");
+                foreach (DataRow dr in drSeats)
+                {
+                    Seat s = new Seat();
+                    s.CurrentDirection = (Seat.Direction)Enum.Parse(typeof(Seat.Direction), dr["Direction"].ToString());
+                    s.CurrentStatus = (Seat.Status)Enum.Parse(typeof(Seat.Status), dr["CurrentStatus"].ToString());
+                    s.Length = int.Parse(dr["SeatLength"].ToString());
+                    s.PositionX = int.Parse(dr["SeatPositionX"].ToString());
+                    s.PositionY = int.Parse(dr["SeatPositionY"].ToString());
+                    s.Width = int.Parse(dr["SeatWidth"].ToString());
+                    s.TableNo = int.Parse(dr["SeatNo"].ToString());
+                    s.SeatNo = int.Parse(dr["SeatNo"].ToString());
+
+                    t.SeatList.Add(s);
+                    Seats.Add(s);
+                }
+
+                Tables.Add(t);
+            }
+        }
 
 
 
