cdefe0ecc46a800c3201efd38612a5d1a46a49b6
xieyuliang <xieyuliang1985@126.com>
2015-08-24 09:23:40 +0800

    啊啊

---
+++ Ryanstaurant.Utility/CommonUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+
+namespace Ryanstaurant.Utility
+{
+    public static class CommonUtil
+    {
+        public static void LoadTypeFromTable(object obj, DataTable dtdataTable)
+        {
+            var type = obj.GetType();
+            var properties = type.GetProperties();
+            foreach (var property in properties.Where(property => dtdataTable.Columns.Contains(property.Name)))
+            {
+                property.SetValue(obj, dtdataTable.Rows[0][property.Name], null);
+            }
+        }
+
+
+
+
+
+    }
+}
