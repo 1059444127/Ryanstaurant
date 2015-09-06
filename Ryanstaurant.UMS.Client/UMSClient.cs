06f484b60b957ae55dd8ca7852a9aa0a6c430a80
xieyuliang <xieyuliang1985@126.com>
2015-09-06 18:12:37 +0800

    update client

---
+++ Ryanstaurant.UMS.Client/UMSClient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using Ryanstaurant.UMS.Interface;
+
+namespace Ryanstaurant.UMS.Client
+{
+    public class UMSClient:IUMSService
+    {
+        private readonly ServiceClient _serviceClient;
+
+
+        public UMSClient(string endpointAddress)
+        {
+            Binding binding;
+            if (endpointAddress.StartsWith("http://"))
+            {
+                binding = new BasicHttpBinding();
+            }
+            else if (endpointAddress.StartsWith("net.tcp://"))
+            {
+                binding = new NetTcpBinding();
+            }
+            else
+            {
+                throw new Exception("Invalid Address Format!");
+            }
+
+            _serviceClient = new ServiceClient(binding, new EndpointAddress(endpointAddress));
+        }
+
+
+
+
+
+        public DataContract.Utility.ResultEntity Execute(List<DataContract.Utility.ItemContent> requestEntitiy)
+        {
+            return _serviceClient.Execute(requestEntitiy);
+        }
+
+        public DataContract.Utility.ResultEntity Query(List<DataContract.Utility.ItemContent> requestEntitiy)
+        {
+            return _serviceClient.Query(requestEntitiy);
+        }
+    }
+}
