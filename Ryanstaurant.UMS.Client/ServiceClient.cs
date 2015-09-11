d91909b2b1e9223f6f7671449d2e4c7852216028
xieyuliang <xieyuliang1985@126.com>
2015-09-11 12:45:01 +0800

    commit

---
+++ Ryanstaurant.UMS.Client/ServiceClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using Ryanstaurant.UMS.Interface;
+
+namespace Ryanstaurant.UMS.Client
+{
+    internal class ServiceClient : ClientBase<IUMSService>, IUMSService
+    {
+        public ServiceClient(Binding binding, EndpointAddress edpAddr)
+            : base(binding, edpAddr)
+        {
+        }
+
+
+        public DataContract.Utility.ResultEntity Execute(List<DataContract.Utility.ItemContent> requestEntitiy)
+        {
+            return Channel.Execute(requestEntitiy);
+        }
+
+        public DataContract.Utility.ResultEntity Query(List<DataContract.Utility.ItemContent> requestEntitiy)
+        {
+            return Channel.Query(requestEntitiy);
+        }
+    }
+}
