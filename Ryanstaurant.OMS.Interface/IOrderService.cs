6ae00b09cfe0addf67223f6fc435b4a799df9585
xieyuliang <xieyuliang1985@126.com>
2015-09-25 17:59:59 +0800

    commit

--- Ryanstaurant.OMS.Interface/IOrderService.cs
+++ Ryanstaurant.OMS.Interface/IOrderService.cs
@@ -6,25 +6,15 @@ namespace Ryanstaurant.OMS.Interface
     public interface IOrderService
     {
         Order Create(Order order, IList<Item> items);
-
         Order Query(string orderId);
-
         Order Change(Order order);
-
         Order Attach(string orderId, IList<Item> items);
-
         Order Revoke(string orderId);
-
         Order CombineOrder(List<Order> orders);
-
         IList<Order> SplitOrder(string orderId, IList<Order> orders);
-
         Bill GetBill(Order order);
-
         Check GetCheck(Bill bill);
-
         Change PayCheck(Check check);
-
         IList<Order> Query(string startDateTime, string endDateTime);
 
     }
